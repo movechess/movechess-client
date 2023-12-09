f7877bb989eda6d9088c017fb6e60a99645a3c05
LeVien <levien.workspace@gmail.com>
2023-12-10 02:59:29 +0700

    update ui

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const token = localStorage.getItem("token");
-export const socket = io("http://localhost:3001", {
+export const socket = io("https://engine.movechess.com", {
   extraHeaders: {
     Authorization: token !== null ? token : "",
   },
