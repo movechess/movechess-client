1f06659863a1acbcf324eab8a6efc653417a989f
LeVien <levien.workspace@gmail.com>
2024-01-30 23:38:23 +0700

    chore: data smc

---
+++ src/utils/data.ts
@@ -0,0 +1,3 @@
+export const TOURNAMENT_CONTRACT_ADDRESS = "5ESfXFfGEtLdgCyPh91eeQgRptgNHVqwLQrXrS4t4YUwXGvF";
+export const MOVECHESS_CONTRACT_ADDRESS = "";
+export const AZ_WSS_URL = "wss://ws.test.azero.dev";
